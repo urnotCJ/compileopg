4bb1916a41ebb5a33884d15872da75e666c68390
IamVincentChen <1798916234@qq.com>
2020-11-03 20:08:44 +0800

    2

--- opg.java
+++ opg.java
@@ -3,8 +3,8 @@ import java.util.ArrayList;
 
 public class opg {
     public static void main(String[] args) throws Exception{
-        //File file = new File(args[0]);
-        File file = new File("src/work.txt");
+        File file = new File(args[0]);
+        //File file = new File("src/work.txt");
         FileReader reader = new FileReader(file);
         BufferedReader breader = new BufferedReader(reader);
         String sb = "";
