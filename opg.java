471aec37e0f35490c09942fc4b7ce12f6dc4c875
IamVincentChen <1798916234@qq.com>
2020-11-03 22:42:16 +0800

    6

--- opg.java
+++ opg.java
@@ -15,8 +15,6 @@ public class opg {
         int c[] = new int[1005];
         a[0] = 0;
         b[0] = 0;
-        int d[] = new int[100];
-        int e[] = new int[100];
         int j = 1;
         while ((sb =breader.readLine()) != null) {
             s = s+sb;
@@ -53,7 +51,7 @@ public class opg {
             }
         }
         a[j]=0;
-        int seta = 1,setb = 0,setc = 0,controla=1,setd=0;
+        int seta = 1,setb = 0,setc = 0,controla=1;
         while(seta<=j&&setb>=0){
             if(fail(a[seta-1],a[seta])==1){
                 setc=1;
@@ -64,7 +62,6 @@ public class opg {
             }
             if(a[seta]==3){
                 System.out.println("Ii");
-                d[setd]++;
                 setc++;
                 seta++;
                 controla=1;
@@ -75,20 +72,20 @@ public class opg {
                     controla=0;
                 }
                 if(power(b[setb],a[seta])==1){
+                    if(setc==0&&a[seta]!=4){
+                        break;
+                    }
                     setb++;
                     b[setb]=a[seta];
                     seta++;
                     if(b[setb]==1){
                         System.out.println("I+");
-                        e[setd]++;
                     }
                     else if (b[setb]==2){
                         System.out.println("I*");
-                        e[setd]++;
                     }
                     else if (b[setb]==4){
                         System.out.println("I(");
-                        setd++;
                     }
                 }
                 else if(power(b[setb],a[seta])==4){
@@ -123,19 +120,6 @@ public class opg {
                                 break;
                             }
                             System.out.println("I)\nR");
-                        }
-                        if(a[seta]==5&&d[setd]-1==e[setd]){
-                            d[setd]=0;
-                            e[setd]=0;
-                            setd--;
-                            d[setd]++;
-                        }
-                        else{
-                            System.out.println("RE");
-                            setc=1;
-                            setb=0;
-                            seta=j;
-                            break;
                         }
                     }
                 }
